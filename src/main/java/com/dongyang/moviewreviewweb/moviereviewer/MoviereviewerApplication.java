a201897e3ca99f03c9e22f9d5416a66a9db50ab1
rdyjun <rdyjun00@gmail.com>
2023-11-08 11:14:26 +0900

    init: first commit

---
+++ src/main/java/com/dongyang/moviewreviewweb/moviereviewer/MoviereviewerApplication.java
@@ -0,0 +1,13 @@
+package com.dongyang.moviewreviewweb.moviereviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviereviewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviereviewerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dongyang/moviewreviewweb/moviereviewer/MoviereviewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongyang.moviewreviewweb.moviereviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviereviewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
