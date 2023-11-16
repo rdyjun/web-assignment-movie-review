06582e9648f006c671ba3c5c209b2857fb4d6dba
rdyjun <rdyjun00@gmail.com>
2023-11-16 13:52:29 +0900

    feat: add DBConnector class
    return Connect value

--- src/main/java/com/dongyang/moviewreviewweb/moviereviewer/DBConnector/DBConnector.java
+++ src/main/java/com/dongyang/moviewreviewweb/moviereviewer/DBConnector/DBConnector.java
@@ -1,2 +1,24 @@
-package com.dongyang.moviewreviewweb.moviereviewer.DBConnector;public class DBConnector {
+package com.dongyang.moviewreviewweb.moviereviewer.DBConnector;
+
+import org.springframework.beans.factory.annotation.Value;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnector {
+    private Connection connect;
+    @Value("${database.driver}")
+    private String dbDriver;
+    @Value("${database.username}")
+    private String userName;
+    @Value("${database.password}")
+    private String password;
+    public DBConnector () throws ClassNotFoundException, SQLException {
+        Class.forName(dbDriver);
+        this.connect = DriverManager.getConnection(this.dbDriver, this.userName, this.password);
+    }
+    public Connection getconnector () {
+        return this.connect;
+    }
 }
